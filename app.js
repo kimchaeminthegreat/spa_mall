3be7e4e8a1db0efe8cf8790154bbb914fe8ee360
KIM, CHAE-MIN <gekcm@naver.com>
2023-04-18 10:40:42 +0900

    it's for test

--- app.js
+++ app.js
@@ -18,3 +18,5 @@ app.use("/api", [goodsRouter, cartsRouter]);
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
 });
+
+
--- schemas/goods.js
+++ schemas/goods.js
@@ -23,3 +23,5 @@ const goodsSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Goods", goodsSchema);
+
+
