c492c752101d2e2330556c3c11f0eda115fbeea9
KIM, CHAE-MIN <gekcm@naver.com>
2023-04-18 09:30:59 +0900

    Frist Commit All Files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require("./routes/carts");
+
+const connect = require("./schemas");
+connect();
+
+app.use(express.json());
+app.use("/api", [goodsRouter, cartsRouter]);
+
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!');
+});
---
+++ modules/run.js
@@ -0,0 +1,3 @@
+const add = require("./math.js");
+
+console.log(add.add(10, 20));
